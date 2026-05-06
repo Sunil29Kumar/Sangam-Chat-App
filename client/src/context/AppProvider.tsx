463f8d3d97044b41e65657e95e0017c13cd2c91e
Sunil Kumar <sunil.kksdk@gmail.com>
2026-05-06 14:02:22 +0530

    fix: socketContext to SocketContext

--- client/src/context/AppProvider.tsx
+++ client/src/context/AppProvider.tsx
@@ -1,6 +1,8 @@
 import AuthProvider from "./AuthContext";
 import ChatProvider from "./ChatContext";
+// name is correct 
 import SocketProvider from "./SocketContext";
+
 
 export function AppProvider({children }: {children: React.ReactNode}) {
   return (
