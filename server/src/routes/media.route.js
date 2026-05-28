3390dad1446038c9cc378554b1e8079dbe36032d
Sunil Kumar <sunil.kksdk@gmail.com>
2026-05-28 18:47:45 +0530

    fix multer path

--- server/src/routes/media.route.js
+++ server/src/routes/media.route.js
@@ -2,11 +2,22 @@ import express from "express";
 import multer from "multer";
 import { speechToText } from "../controllers/media.controller.js";
 
+// const upload = multer({
+//     dest: "uploads/",
+//     limits: {
+//         fileSize: 5 * 1024 * 1024, // 5 MB
+//     },
+// });
+
+
+const uploadDir = "uploads/";
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const upload = multer({
-    dest: "uploads/",
-    limits: {
-        fileSize: 5 * 1024 * 1024, // 5 MB
-    },
+    dest: uploadDir,
+    limits: { fileSize: 5 * 1024 * 1024 } // 5MB safety limit
 });
 
 const router = express.Router()
